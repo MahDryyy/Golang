e037015fc10abc488c3e726fd19edaf433eec5f5
Yesthon31 <yestondehaan607@gmail.com>
2025-05-07 14:23:39 +0700

    baru

---
+++ db/db.go
@@ -0,0 +1,334 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/generative-ai-go/genai"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/api/option"
+)
+
+var DB *sql.DB
+var jwtSecret = []byte("your_secret_key")
+
+type Food struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	ExpiryDate string `json:"expiry_date"`
+}
+
+type AddFoodRequest struct {
+	Name       string `json:"name"`
+	ExpiryDate string `json:"expiry_date"`
+}
+
+type RecipeRequest struct {
+	FoodID int `json:"food_id"`
+}
+
+type RecipeResponse struct {
+	Recipe string `json:"recipe"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func InitDB() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	var err error
+	DB, err = sql.Open("mysql", databaseURL)
+	if err != nil {
+		log.Fatalf("Gagal terhubung ke database: %v", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Tidak bisa terhubung ke database: %v", err)
+	}
+
+	fmt.Println("✅ Berhasil terhubung ke database")
+}
+
+func AddFood(name, expiryDate, userId string) error {
+	_, err := DB.Exec("INSERT INTO foods (name, expiry_date, user_id) VALUES (?, ?, ?)", name, expiryDate, userId)
+	return err
+}
+
+// Function to generate JWT token
+func GenerateJWT(username string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+			Issuer:    "myapp",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func GetFoods(userId string) ([]Food, error) {
+	rows, err := DB.Query("SELECT id, name, expiry_date FROM foods WHERE user_id = ?", userId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var foods []Food
+	for rows.Next() {
+		var food Food
+		if err := rows.Scan(&food.ID, &food.Name, &food.ExpiryDate); err != nil {
+			return nil, err
+		}
+		foods = append(foods, food)
+	}
+
+	return foods, nil
+}
+
+func AddFoodRecipe(foodID int, recipe, userId string) error {
+	_, err := DB.Exec("INSERT INTO food_recipes (food_id, recipe, user_id) VALUES (?, ?, ?)", foodID, recipe, userId)
+	return err
+}
+
+func GetFoodRecipes(userId string) ([]string, error) {
+	rows, err := DB.Query("SELECT recipe FROM food_recipes WHERE user_id = ?", userId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var recipes []string
+	for rows.Next() {
+		var recipe string
+		if err := rows.Scan(&recipe); err != nil {
+			return nil, err
+		}
+		recipes = append(recipes, recipe)
+	}
+
+	return recipes, nil
+}
+
+func GenerateRecipe(foodName string) (string, error) {
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatal("API key tidak ditemukan! Pastikan sudah diset di environment variable.")
+	}
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey))
+	if err != nil {
+		log.Fatalf("Error creating AI client: %v", err)
+	}
+	defer client.Close()
+
+	userInput := fmt.Sprintf("anggap dirimu adalah chef. Berikan resep gampang dan berikan ukuran pasti tapi enak untuk: %s. Di terakhir tuliskan by Chef SaveBite", foodName)
+	model := client.GenerativeModel("gemini-1.5-flash")
+	resp, err := model.GenerateContent(ctx, genai.Text(userInput))
+	if err != nil {
+		return "", err
+	}
+
+	if len(resp.Candidates) == 0 || resp.Candidates[0].Content == nil {
+		return "", fmt.Errorf("AI tidak mengembalikan hasil yang valid")
+	}
+
+	var output strings.Builder
+	for _, part := range resp.Candidates[0].Content.Parts {
+		output.WriteString(fmt.Sprintf("%v\n", part))
+	}
+
+	return output.String(), nil
+}
+
+func ValidateToken(c *gin.Context) {
+	tokenString := c.GetHeader("Authorization")
+	if tokenString == "" {
+		c.JSON(401, gin.H{"error": "Token tidak ditemukan"})
+		c.Abort()
+		return
+	}
+
+	tokenString = tokenString[7:]
+
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("invalid signing method")
+		}
+		return jwtSecret, nil
+	})
+
+	if err != nil || !token.Valid {
+		c.JSON(401, gin.H{"error": "Invalid token"})
+		c.Abort()
+		return
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		c.Set("username", claims.Username)
+
+		var userId, role string
+		err := DB.QueryRow("SELECT id, role FROM users WHERE username = ?", claims.Username).Scan(&userId, &role)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+			c.Abort()
+			return
+		}
+
+		c.Set("user_id", userId)
+		c.Set("role", role)
+		c.Next()
+	} else {
+		c.JSON(401, gin.H{"error": "Invalid token"})
+		c.Abort()
+	}
+}
+
+func LoginHandler(c *gin.Context) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	var storedPasswordHash string
+	err := DB.QueryRow("SELECT password FROM users WHERE username = ?", req.Username).Scan(&storedPasswordHash)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(storedPasswordHash), []byte(req.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token, err := GenerateJWT(req.Username)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	c.JSON(200, gin.H{"token": token})
+}
+
+func RegisterHandler(c *gin.Context) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+		Role     string `json:"role"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	if req.Role != "user" && req.Role != "admin" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Role must be 'admin' or 'user'"})
+		return
+	}
+
+	var existingUsername string
+	err := DB.QueryRow("SELECT username FROM users WHERE username = ?", req.Username).Scan(&existingUsername)
+	if err == nil {
+		c.JSON(http.StatusConflict, gin.H{"error": "Username already exists"})
+		return
+	}
+
+	hashedPassword, err := hashPassword(req.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	_, err = DB.Exec("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", req.Username, hashedPassword, req.Role)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
+}
+
+func hashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func GetFoodName(foodID int, userId string) (string, error) {
+	var foodName string
+	err := DB.QueryRow("SELECT name FROM foods WHERE id = ? AND user_id = ?", foodID, userId).Scan(&foodName)
+	if err != nil {
+		return "", err
+	}
+	return foodName, nil
+}
+func GetUsers() ([]map[string]interface{}, error) {
+	rows, err := DB.Query("SELECT id, username, role FROM users")
+	if err != nil {
+		log.Printf("Error executing query: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []map[string]interface{}
+	for rows.Next() {
+		var userId, username, role string
+		if err := rows.Scan(&userId, &username, &role); err != nil {
+			log.Printf("Error scanning row: %v", err)
+			return nil, err
+		}
+		user := map[string]interface{}{
+			"id":       userId,
+			"username": username,
+			"role":     role,
+		}
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("Error iterating over rows: %v", err)
+		return nil, err
+	}
+
+	return users, nil
+}
---
+++ main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"api/db"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	db.InitDB()
+
+	r := gin.Default()
+
+	r.POST("/register", db.RegisterHandler)
+	r.POST("/login", db.LoginHandler)
+
+	r.GET("/recipes", db.ValidateToken, func(c *gin.Context) {
+		userId := c.MustGet("user_id").(string)
+
+		recipes, err := db.GetFoodRecipes(userId)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mengambil resep"})
+			return
+		}
+
+		if len(recipes) == 0 {
+			c.JSON(http.StatusOK, gin.H{"message": "Tidak ada resep yang ditemukan"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"recipes": recipes})
+	})
+
+	r.GET("/users", db.ValidateToken, func(c *gin.Context) {
+		role := c.MustGet("role").(string)
+
+		if role != "admin" {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Anda tidak memiliki izin untuk mengakses daftar pengguna"})
+			return
+		}
+
+		users, err := db.GetUsers()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mengambil data pengguna"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"users": users})
+	})
+
+	r.POST("/foods", db.ValidateToken, func(c *gin.Context) {
+		var req db.AddFoodRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Format request salah"})
+			return
+		}
+
+		userId := c.MustGet("user_id").(string)
+		err := db.AddFood(req.Name, req.ExpiryDate, userId)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal menyimpan makanan"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Makanan berhasil disimpan"})
+	})
+
+	r.DELETE("/recipes/:id", db.ValidateToken, func(c *gin.Context) {
+		id := c.Param("id")
+		userId := c.MustGet("user_id").(string)
+
+		var recipeOwnerId string
+		err := db.DB.QueryRow("SELECT user_id FROM food_recipes WHERE id = ?", id).Scan(&recipeOwnerId)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Resep tidak ditemukan"})
+			return
+		}
+
+		if recipeOwnerId != userId {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Resep ini bukan milik Anda"})
+			return
+		}
+
+		_, err = db.DB.Exec("DELETE FROM food_recipes WHERE id = ?", id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal menghapus resep"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Resep berhasil dihapus"})
+	})
+
+	
+
+	r.DELETE("/foods/:id", db.ValidateToken, func(c *gin.Context) {
+		id := c.Param("id")
+		userId := c.MustGet("user_id").(string)
+
+		var foodUserId string
+		err := db.DB.QueryRow("SELECT user_id FROM foods WHERE id = ?", id).Scan(&foodUserId)
+		if err != nil || foodUserId != userId {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Makanan tidak ditemukan atau tidak milik Anda"})
+			return
+		}
+
+		_, err = db.DB.Exec("DELETE FROM foods WHERE id = ?", id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal menghapus makanan"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Makanan berhasil dihapus"})
+	})
+
+	r.GET("/foods", db.ValidateToken, func(c *gin.Context) {
+		userId := c.MustGet("user_id").(string)
+
+		foods, err := db.GetFoods(userId)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mengambil data makanan"})
+			return
+		}
+		c.JSON(http.StatusOK, foods)
+	})
+
+	r.POST("/recipe", db.ValidateToken, func(c *gin.Context) {
+		var req db.RecipeRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Format request salah"})
+			return
+		}
+
+		userId := c.MustGet("user_id").(string)
+
+		var foodName string
+		err := db.DB.QueryRow("SELECT name FROM foods WHERE id = ? AND user_id = ?", req.FoodID, userId).Scan(&foodName)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Makanan tidak ditemukan atau tidak milik Anda"})
+			return
+		}
+
+		apiKey := os.Getenv("API_KEY")
+		if apiKey == "" {
+			log.Fatal("API key tidak ditemukan! Pastikan sudah diset di environment variable.")
+		}
+		ctx := context.Background()
+		client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey))
+		if err != nil {
+			log.Fatalf("Error creating AI client: %v", err)
+		}
+		defer client.Close()
+
+		userInput := fmt.Sprintf("anggap dirimu adalah chef. Berikan resep gampang dan berikan ukuran pasti tapi enak untuk: %s. Di terakhir tuliskan by Chef SaveBite", foodName)
+
+		model := client.GenerativeModel("gemini-1.5-flash")
+		resp, err := model.GenerateContent(ctx, genai.Text(userInput))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mendapatkan resep dari AI"})
+			return
+		}
+
+		if len(resp.Candidates) == 0 || resp.Candidates[0].Content == nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "AI tidak mengembalikan hasil yang valid"})
+			return
+		}
+
+		var output strings.Builder
+		for _, part := range resp.Candidates[0].Content.Parts {
+			output.WriteString(fmt.Sprintf("%v\n", part))
+		}
+
+		err = db.AddFoodRecipe(req.FoodID, output.String(), userId)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal menyimpan resep ke database"})
+			return
+		}
+
+		c.JSON(http.StatusOK, db.RecipeResponse{Recipe: output.String()})
+	})
+
+	r.Run(":8080")
+}
